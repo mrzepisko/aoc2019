a1be9968cf39d67d9779574a6e81d55c3e2245de
mrzepisko <rz.marcin@gmail.com>
2019-12-14 20:41:00 +0000

    day 09

---
+++ AdventOfCode2019-Cs/Day09.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Schema;
+using AdventOfCode2019.Intcode;
+using AdventOfCode2019.Intcode.Commands;
+using AdventOfCode2019.Intcode.Wires;
+
+namespace AdventOfCode2019
+{
+    public class Day09 : Day
+    {
+        public override string Name => "--- Day 9: Sensor Boost ---";
+        public override string Input => InputResources.Day09;
+
+        private Processor processor;
+        private InteropProgram program;
+
+
+        public Day09()
+        {
+            processor = Processor.CreateBasic();
+        }
+
+        public override void ParseInput(string input)
+        {
+            program = new InteropProgram(input);
+        }
+
+        public override string PartOne()
+        {
+            Context ctx = program.Prepare();
+            ctx.Input.Write(1);
+            int result = processor.Execute(ctx);
+            return result.ToString();
+        }
+
+        public override string PartTwo()
+        {
+            Context ctx = program.Prepare();
+            ctx.Input.Write(5);
+            int result = processor.Execute(ctx);
+            return result.ToString();
+        }
+    }
+}
